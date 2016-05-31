f32342249b97e9195de70926b6f7975b26793fe9
Laxmi Tulasi Vaddiapti <laxmi.vaddipati@valuelabs.com>
2016-05-31 16:07:16 +0530

    initial commit

---
+++ src/containers/book-list.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {selectBook } from '../actions/index'
+import {bindActionCreators} from 'redux'
+
+class BookList extends Component {
+	renderList() {
+		return this.props.books.map((book) => {
+			return (
+				<li 
+					onClick={() => this.props.selectBook(book)} 
+					key={book.title} 
+					className="list-group-item">
+					{book.title}
+				</li>
+			);
+		});
+	}
+	render() {
+		return (
+			<ul className="list-froup col-sm-4">
+				{this.renderList()}
+			</ul>
+		)
+	}
+}
+
+function mapStateToProps(state){
+	return {
+		books:state.books
+	};
+}
+
+function mapDisptachToProps(dispatch) {
+	return bindActionCreators ({ selectBook:selectBook }, dispatch)
+}
+export default connect(mapStateToProps, mapDisptachToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return [
+		{ title: 'Book1' , pages: 108},
+		{ title: 'Book2' , pages: 38},
+		{ title: 'Book3' , pages: 88},
+		{ title: 'Book4' , pages: 58},
+		{ title: 'Book5' , pages: 10}
+	];
+}
